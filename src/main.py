7d2c5dfcf0e2ac659782828c2eed8f1f412f8c4d
wr786 <wr786@pku.edu.cn>
2020-05-27 21:39:09 +0800

    Added Highlight & Message area

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
 # author: wr786
 from flask import Flask, session, render_template, request, redirect
 import os
+from pygments import highlight
+from pygments.formatters.html import HtmlFormatter
+from pygments.lexers import get_lexer_by_name
+from pygments.lexers.python import PythonLexer
+import time
+
+lexer = get_lexer_by_name("python")
+formatter = HtmlFormatter(style="xcode")
 
 app = Flask(__name__)
+
+@app.route("/highlight", methods=["POST", "GET"])
+def addHighlight():
+    code = request.form["code"]
+    html = highlight(code, lexer, formatter)
+    return html
+
+@app.route("/runScript", methods=["POST", "GET"])
+def runScript():
+    code = request.form["code"]
+    return "OK"
 
 @app.route("/")
 def index() :
