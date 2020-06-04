368e994a0dcf62ca0211ba8eed18d8ca4d1017c7
wr786 <wr786@pku.edu.cn>
2020-06-04 22:43:13 +0800

    fixed delete

--- src/data/code.py
+++ src/data/code.py
@@ -3,17 +3,4 @@
 import sys
 f_handler = open('./data/in.log', 'r')
 sys.stdin = f_handler
-#author: wr786
-
 print("Hello, My Happy World!")
-
-str = """
-POPIPA
-PIPOPA
-POPIPAPAPIPOPA!
-"""
-
-def fn():
-    print(str)
-    
-fn()
