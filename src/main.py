7fecbc3fb4c6e55347b1b0f05280d1f80d40ffe9
wr786 <wr786@pku.edu.cn>
2020-06-06 15:39:10 +0800

    Upload & Share & Download

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # author: wr786
-from flask import Flask, session, render_template, request, redirect
+from flask import Flask, session, render_template, request, redirect, send_from_directory
 from pygments import highlight
 from pygments.formatters.html import HtmlFormatter
 from pygments.lexers import get_lexer_by_name
@@ -15,6 +15,19 @@ formatter = HtmlFormatter(style="colorful")
 
 app = Flask(__name__)
 
+@app.route("/upload", methods=["POST", "GET"])
+def uploadCode():   # 上传代码文件，用来交流分享
+    code = request.form["code"]
+    timestamp = request.form["timestamp"]
+    with open(f'./data/code_{timestamp}.py', "w", encoding="utf-8") as f:
+        f.write(code)
+    # os.rename(f'./data/code_{timestamp}.txt', f'./data/code_{timestamp}.py')
+    return f'code_{timestamp}.py'   # 返回文件名，用来和下载进行交互
+
+@app.route("/download/<filename>", methods=['GET']) # 下载代码
+def downloadFile(filename):
+    return send_from_directory('./data/', filename, as_attachment=True)
+
 @app.route("/highlight", methods=["POST", "GET"])
 def addHighlight(): # 给代码加上高亮，运用pygments
     code = request.form["code"]
@@ -25,26 +38,27 @@ def addHighlight(): # 给代码加上高亮，运用pygments
 def runScript():    # 运行代码
     code = request.form["code"]
     inputData = request.form["inputData"]
+    timestamp = request.form["timestamp"]
     code = """#!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import sys
 f_handler = open('./data/in.log', 'r')
 sys.stdin = f_handler
 """ + code
-    with open('./data/in.log', "w", encoding="utf-8") as f:
+    with open(f'./data/in_{timestamp}.log', "w", encoding="utf-8") as f:
         f.write(inputData)
-    with open('./data/code.py', "w", encoding="utf-8") as f:
+    with open(f'./data/code_{timestamp}.py', "w", encoding="utf-8") as f:
         f.write(code)
     try:
         process = subprocess.run('python ./data/code.py',shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding="utf-8",timeout=1)
-        os.remove('./data/in.log')
-        os.remove('./data/code.py')
+        os.remove(f'./data/in_{timestamp}.log')
+        os.remove(f'./data/code_{timestamp}.py')
         if process.returncode == 0:
             # "\n".join([line.decode('cp936').encode('utf-8') for line in process.stdout.readlines()])
             return "0" + process.stdout
         else:
             return "1" + process.stderr
-    except Exception as e:
+    except Exception:
         return "1" + "Unknown Error:\nMaybe you used some illegal character in your code, you can try replacing them.\nOr maybe it's just a TLE.\n"
 
 @app.route("/")
